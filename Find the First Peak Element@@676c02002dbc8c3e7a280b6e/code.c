8c2c16cb1bbaf14911c282b388edce8f85ccbeda
Angel Sharma <angelsharma19972006@gmail.com>
2025-03-27 16:23:08 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -1,31 +1,72 @@
-#include<stdio.h>
-int peak(int arr[],int n){
-    if (n==1){
-        return arr[0];
-    }
-    if(arr[0]>arr[1]){
-        return arr[0];
-    }
-    for(int i=1;i<n-2;i++){
-        if(arr[i]>arr[i+1] && arr[-1]<arr[i]){
-            printf("%d",arr[i]);
-            break;
-        }  
-    }
-    if(arr[n-1]>arr[n-2]){
-        return arr[n-1];
-    }
-    return -1;
+// #include<stdio.h>
+// int peak(int arr[],int n){
+//     if (n==1){
+//         return arr[0];
+//     }
+//     if(arr[0]>arr[1]){
+//         return arr[0];
+//     }
+//     for(int i=1;i<n-2;i++){
+//         if(arr[i]>arr[i+1] && arr[-1]<arr[i]){
+//             printf("%d",arr[i]);
+//             break;
+//         }  
+//     }
+//     if(arr[n-1]>arr[n-2]){
+//         return arr[n-1];
+//     }
+//     return -1;
     
-}
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d ",&arr[i]);   
+// }
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         scanf("%d ",&arr[i]);   
+//     }
+//     int p=peak(arr,n);
+//     printf("%d",p);
+//     return 0;
+// }
+
+#include<stdio.h>
+
+int peak(int arr[], int n) {
+    if (n == 1) {
+        return arr[0];
     }
-    int p=peak(arr,n);
-    printf("%d",p);
+    
+    // Check first element
+    if (arr[0] > arr[1]) {
+        return arr[0];
+    }
+
+    // Check middle elements
+    for (int i = 1; i < n - 1; i++) {
+        if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+            return arr[i];  // Return first peak found
+        }
+    }
+
+    // Check last element
+    if (arr[n - 1] > arr[n - 2]) {
+        return arr[n - 1];
+    }
+
+    return -1;  // No peak found
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);  // Removed extra space
+    }
+    
+    int p = peak(arr, n);
+    printf("%d", p);
     return 0;
 }
