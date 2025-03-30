a3cbadeceb5b929ad7f1ce1debac13b674230c52
Angel Sharma <angelsharma19972006@gmail.com>
2025-03-30 20:33:36 +0530

    Solution to Kth Missing Positive Number

---
+++ Kth Missing Positive Number@@67ca7acd1fe08d7207c253c5/code.c
@@ -0,0 +1,15 @@
+// Your code here...
+#include<stdio.h>
+int main(){
+    int a,b;
+    scanf("%d\n",&a);
+    for(int i=0;i<a;i++){
+        scanf("%d ",arr[i]);
+    }
+    scanf("%d\n",&b);
+    for(int i=1;i<=arr[a-1];i++){
+        if(i==arr[i])
+        continue;
+    }
+    return 0;
+}
