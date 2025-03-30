98af5c182fd2617b36424e947976a6f09e5525c8
Angel Sharma <angelsharma19972006@gmail.com>
2025-03-30 19:36:14 +0530

    Solution to Selection Sort for Integers

---
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -0,0 +1,21 @@
+// Your code here...
+// Your code here...
+void insertionSort(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        int last=n-i-1;
+        int maxIndex=0;
+        for(int j=1;j<=last;j++){
+            if(arr[j]>arr[maxIndex]){
+                maxIndex=j;
+            }
+        }
+        int temp=arr[maxIndex];
+        arr[maxIndex]=arr[last];
+        arr[maxIndex]=temp;
+    }
+}
+int printArray(int arr[],int n){
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+}
